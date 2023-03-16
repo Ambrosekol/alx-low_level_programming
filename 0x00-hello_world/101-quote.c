64ed9b7f30dce24f50b063157cea9cd6c047c9d8
ambrose <kolus7381@gmail.com>
2023-03-16 18:37:45 +0100

    new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
     fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-    return (0);
+    return (1);
 }
