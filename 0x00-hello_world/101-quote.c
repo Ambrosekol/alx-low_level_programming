5b8bc34b342f978a528e26b01c525ebfef60d639
ambrose <kolus7381@gmail.com>
2023-03-16 18:56:43 +0100

    new commuit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\"");
-	fprintf(stderr, "- Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
