8bf02bbe0f70abd1194786bc8f841b9f9f53b827
ambrose <kolus7381@gmail.com>
2023-03-16 17:03:57 +0100

    new commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Code starts here
+  *
+  * Return (0) success
+  */
+int main(void)
+{
+	int a = sizeof(char);
+	int b = sizeof(int);
+	int c = sizeof(long int);
+	int d = sizeof(long long int);
+	int e = sizeof(float);
+
+	printf("Size of a char: %d byte(s)\n", a);
+	printf("Size of a int: %d byte(s)\n", b);
+	printf("size of a long int: %d byte(s)\n", c);
+	printf("size of a long long int: %d byte(s)\n", d);
+	printf("size of a float: %d byte(s)\n", e);
+	return (0);
+}
