b0162619debe6186726a6a1ec7f4a5014bc33a66
ambrose <kolus7381@gmail.com>
2023-03-16 16:33:22 +0100

    new commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  *this is the main entry point
+  * i am using the betty style
+  **/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+
+	return 0;
+}
