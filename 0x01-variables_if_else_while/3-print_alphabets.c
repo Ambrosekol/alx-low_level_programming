278dbf8e88207460f5459e23c049481a22f898ae
ambrose <kolus7381@gmail.com>
2023-03-17 16:21:59 +0100

    new commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of the code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ca;
+
+	char sa;
+
+	for (sa = 'a'; sa <= 'z'; sa++)
+	{
+		putchar(sa);
+	}
+	for (ca = 'A'; ca = <= 'Z'; ca++)
+	{
+		putchar(ca);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
