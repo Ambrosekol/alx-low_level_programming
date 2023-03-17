e724eae7dd2c2dfad1b126c969ce780f7eb48872
ambrose <kolus7381@gmail.com>
2023-03-17 16:39:14 +0100

    new commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Starting point of the code
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int ca;
+
+	for (ca = 0; ca <= 5; ca++)
+	{
+		putchar(ca);
+	}
+	putchar('\n');
+
+	return (0);
+}
