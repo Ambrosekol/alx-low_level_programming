83a6ef3fa2e82f49b9d939018718a4571befbed4
ambrose <kolus7381@gmail.com>
2023-03-17 16:02:16 +0100

    new commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - Starting point of the code
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	for (char ca = 'a'; ca < 'z'; ca++)
+	{
+		putchar(ca);
+	}
+
+	return (0);
+}
