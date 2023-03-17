96c9a1aaa444b495e265ac728c4aae8f650f6e5d
ambrose <kolus7381@gmail.com>
2023-03-17 17:03:31 +0100

    new commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - Starting point of the code
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int ca;
+
+	for (ca = 0; ca < 10; ca++)
+	{
+		putchar(ca + '0');
+
+		if (ca != 9)
+		{
+			putchar(',');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
