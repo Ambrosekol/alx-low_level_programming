77484f59c3e95285ea35805e4c1edf59861ac479
Ambrose <kolus7381@gmail.com>
2023-03-21 17:04:59 +0100

    new commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * _isalpha - checks if int c is higher
+  *
+  * @c: number to be checked
+  *
+  * Return: Success (0) or (1)
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		if (n = 0)
+		{
+			_putchar('0');
+			return (0);
+		}
+		else
+		{
+			if ( n < 0)
+			{
+				_putchar('-');
+				return (-1);
+			}
+		}
+	}
+}
