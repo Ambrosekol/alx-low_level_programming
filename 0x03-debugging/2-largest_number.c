dcc03ffe5aaa0d1d99488b63808f1cc099fdb05e
Ambrose <kolus7381@gmail.com>
2023-03-22 15:14:29 -0700

    new commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,13 +20,9 @@ int largest_number(int a, int b, int c)
     {
         largest = c;
     }
-    else if (b > c)
-    {
-        largest = b;
-    }
     else
     {
-	    largest = c;
+	    largest = b;
     }
 
     return (largest);
