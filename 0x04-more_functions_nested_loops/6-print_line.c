1359da0e3bff0d37934ebe396f7f50f24f78ced8
Ambrose <kolus7381@gmail.com>
2023-03-23 03:24:19 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ * @n: number of lines to draw
+ */
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
