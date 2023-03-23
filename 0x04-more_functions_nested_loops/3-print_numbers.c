d914e74400e5b1e08ea568dc6d9b869a9ebb17ea
Ambrose <kolus7381@gmail.com>
2023-03-23 02:49:42 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 - 9
+ *
+ */
+void print_numbers(void)
+{
+	int val;
+
+	for (val = 0; val < 10; val++)
+	{
+		_putchar(val);
+	}
+	_putchar('\n');
+}
