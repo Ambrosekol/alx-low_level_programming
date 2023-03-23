965d646232aab65f3202e1b8ae8e888b2bbf01c8
ambrose <kolus7381@gmail.com>
2023-03-23 17:23:28 +0100

    new commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * print_triangle - prints a triangle
+  * @size: size of triangle
+  */
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
