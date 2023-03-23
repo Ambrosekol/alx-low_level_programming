417fe484373dc4fe61ea1a7e3d74804e09c05582
Ambrose <kolus7381@gmail.com>
2023-03-23 02:31:10 -0700

    new commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code
+ * @c: value to be tested
+ * Return: either 0 or 1
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 57)
+		return (1);
+	return (0);
+}
