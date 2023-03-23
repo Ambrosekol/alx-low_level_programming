a4388659baef4c932cb01bc203499fdb981c6ac9
ambrose <kolus7381@gmail.com>
2023-03-23 15:17:51 +0100

    new commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - starting point of the code
+  *
+  * Return: Success (0)
+  */
+int main(void)
+{
+	int c;
+
+	for (c = 1; c <= 100; c++)
+	{
+		if ((c % 3 == 0) && (c % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (c % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (c % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", c);
+		}
+	}
+	printf("\n");
+	return (0);
+}
