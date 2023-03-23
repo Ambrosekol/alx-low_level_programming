bb3e752d74e88af046ab5891af5a66fe13699b0d
ambrose <kolus7381@gmail.com>
2023-03-23 14:57:00 +0100

    new commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,12 +8,19 @@ void print_square(int size)
 {
 	int a, b;
 
-	for (a = 0; a < size; a++)
+	if(size != 0)
 	{
-		for (b = 0; b < size; b++)
+		for (a = 0; a < size; a++)
 		{
-			_putchar('#');
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
