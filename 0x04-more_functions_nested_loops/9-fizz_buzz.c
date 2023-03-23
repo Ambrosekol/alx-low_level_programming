2799f2bd5a70810213be0a10ae3814d7dee70221
ambrose <kolus7381@gmail.com>
2023-03-23 15:39:53 +0100

    new commit

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,25 +11,25 @@ int main(void)
 
 	for (c = 1; c <= 100; c++)
 	{
-		if (c % 3 == 0 && c % 5 == 0)
-		{
-			printf(" FizzBuzz");
-		}
-		else if (c % 5 == 0 && c & 3 != 0)
+		if (c % 3 == 0 && c % 5 != 0)
 		{
 			printf(" Fizz");
 		}
-		else if (c % 3 == 0 && c % 5 != 0)
+		else if (c % 5 == 0 && c & 3 != 0)
 		{
 			printf(" Buzz");
 		}
+		else if (c % 3 == 0 && c % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
 		else if (c == 1)
 		{
-			printf("%d ", c);
+			printf("%d", c);
 		}
 		else
 		{
-			printf("%d ", c);
+			printf(" %d", c);
 		}
 	}
 	printf("\n");
