ace1ae04ad836718c37999bcf4a26584d3b4f275
ambrose <kolus7381@gmail.com>
2023-03-27 20:03:21 +0100

    new commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _strcpy - writes a string that copies the string
+  * @dest: destination
+  * @src: source
+  * Return: Char type
+  */
+char *_strcpy(char *dest, char *src)
+{
+	char midary[1000];
+
+	strcpy(midary, src);
+	dest = midary;
+	return midary;
+}
