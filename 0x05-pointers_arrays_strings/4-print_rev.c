a259723e6dfef8458907865e762c51e52ed5d4a4
ambrose <kolus7381@gmail.com>
2023-03-27 17:13:28 +0100

    new commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * print_rev - this prints a string in reverse
+  * @s: parameter that initiates the functions
+  * Return: void (no return value)
+  */
+void print_rev(char *s)
+{
+	int a;
+	char revstr[strlen(s)-1];
+	int b = 0;
+
+	for (a = (strlen(s) - 1); a >= 0; a--)
+	{
+		revstr[b] = s[a];
+		b++;
+	}
+}
