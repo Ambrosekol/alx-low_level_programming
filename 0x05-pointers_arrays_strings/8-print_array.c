a9e8df8adcd192b8457d8fbd90a25311e3654b59
ambrose <kolus7381@gmail.com>
2023-03-27 19:36:08 +0100

    new commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,6 +11,13 @@ void print_array(int *a, int n)
 
 	for (z = 0; z < n; z++)
 	{
-		printf("%d, ", a[z]);
+		if (a[z] != a[n])
+		{
+			printf("%d, ", a[z]);
+		}
+		else
+		{
+			print("%d '\n'", a[z]);
+		}
 	}
 }
