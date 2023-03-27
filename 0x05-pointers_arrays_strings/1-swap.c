3db11442b84d3dd9053fb0bc0bba4d9a7cfa464d
Ambrose <kolus7381@gmail.com>
2023-03-27 14:40:00 +0100

    new function added

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * swap_int - swap intiger of two values
+ * @a: int value a
+ * @b: int value b
+ * return: void
+ */
+void swap_int(int *a, int *b)
+{
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 int _putchar(char);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #endif
