3bb54340f763fc691ae8c4d6e51a4fdb0ca979d9
ambrose <kolus7381@gmail.com>
2023-03-27 18:29:30 +0100

    new commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * puts_half - prints half of the array
+  * @str: entry point of function
+  * Return: void function
+  */
+void puts_half(char *str)
+{
+	int a;
+
+	int z = (strlen(str) - 1) / 2;
+	for (a = 0; a <= ((int) strlen(str)); a++)
+	{
+		printf("%c", str[0 + z]);
+	}
+}
