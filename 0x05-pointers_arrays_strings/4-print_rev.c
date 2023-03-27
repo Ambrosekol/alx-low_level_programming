9651568c0049d8cc6654aefb8fa94b549f7372a9
ambrose <kolus7381@gmail.com>
2023-03-27 17:21:24 +0100

    new commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,7 +7,7 @@
 void print_rev(char *s)
 {
 	int a;
-	char revstr[];
+	char revstr = {};
 	int b = 0;
 
 	for (a = (strlen(s) - 1); a >= 0; a--)
@@ -15,4 +15,5 @@ void print_rev(char *s)
 		revstr[b] = s[a];
 		b++;
 	}
+	puts(revstr);
 }
