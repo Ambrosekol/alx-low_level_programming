01041664e7581ca3795f59be461e39f25fcab1ab
Ambrose <kolus7381@gmail.com>
2023-03-27 15:29:35 +0100

    new commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - length of a string
+ * @s: character to use
+ * return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	a = strlen(*s);
+	return (a);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <string.h>
+
 int _putchar(char);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif
