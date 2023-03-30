8e95b5643d828d05d14ae849f8a5bf97c25030ff
ambrose <kolus7381@gmail.com>
2023-03-30 19:10:41 +0100

    new commit

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,10 +9,10 @@ void reverse_array(int *a, int n)
 {
 	int i, temp;
 
-	for(i = 0; i < n/2; i++)
+	for(i = 0; i < n--; i++)
 	{
 		temp = a[i];
-		a[i] = a[n - 1 - i];
-		a[n - 1 - i] = temp;
+		a[i] = a[n];
+		a[n] = temp;
 	}
 }
