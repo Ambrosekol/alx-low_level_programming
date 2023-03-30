d6bc2e74a7d673881ac9a05825d4bb7febab9e56
ambrose <kolus7381@gmail.com>
2023-03-30 19:37:55 +0100

    new commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * leet - encodes string into 1337
+  * @n: input
+  * Return: char
+  */
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; j < 10; j++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+				n[i] = s2[j];
+		}
+	}
+	return (n);
+}
