de113b21c4aa9b9aaf93ab0d2936fe2af3404386
ambrose <kolus7381@gmail.com>
2023-03-30 17:53:54 +0100

    new commi

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * _strcmp - compares two strings
+  * @s1: string 1
+  * @s2: string 2
+  * Return: returns an Intiger
+  */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = strcmp(s1, s2);
+	return (a);
+}
