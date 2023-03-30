9fdee61e422b6d3acca15c30b2b0ad50e588ce3a
ambrose <kolus7381@gmail.com>
2023-03-30 17:46:30 +0100

    new commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,11 +8,8 @@
   */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	char *p;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = '\0';
-	}
-	return (dest);
+	p = strncpy(dest, src, n);
+	return (p);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+
+#include <string.h>
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
