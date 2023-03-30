9a7c5baf6a42522e842b8abd580c14062a7f32ce
ambrose <kolus7381@gmail.com>
2023-03-30 19:22:10 +0100

    new commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+  * cap_string - cap all words of a string
+  * @n: input
+  * Return: char
+  */
+char *cap_string(char *n)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(n[index] >= 'a' && n[index] <= 'z'))
+			index ++;
+		if (n[index - 1] == ' ' ||
+				n[index - 1] == '\t' ||
+				n[index - 1] == '\n' ||
+				n[index - 1] == '.'||
+				n[index - 1] == ';' ||
+				n[index - 1] == ',' ||
+				n[index - 1] == '!' ||
+				n[index - 1] == '?' ||
+				n[index - 1] == '"' ||
+				n[index - 1] == '(' ||
+				n[index - 1] == ')' ||
+				n[index - 1] == '{' ||
+				n[index - 1] == '}' ||
+				index == 0)
+			n[index] -= 32;
+		index++;
+	}
+	return (n);
+}
