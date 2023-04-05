d8da91b9026d408f1d7cbf9fcdad7a8ce2caf7bf
ambrose <kolus7381@gmail.com>
2023-04-05 18:02:19 +0100

    new commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * _strlen_recursion -  a function that returns the length of a string
+  * @s: input
+  * Return: void
+  */
+int _strlen_recursion(char *s)
+{
+	int a = strlen(s);
+	return a;
+}
