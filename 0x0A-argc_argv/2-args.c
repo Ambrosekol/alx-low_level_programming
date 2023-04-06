ccb649cf3d75f63ce494493d9dc90cbbfd91a890
ambrose <kolus7381@gmail.com>
2023-04-06 17:47:36 +0100

    new commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * main - main point of program
+  * @argc: argument count 
+  * @argv: argument vector of strings
+  * Return: success
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
