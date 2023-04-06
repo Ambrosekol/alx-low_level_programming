8c86b41ebff922791aa972c3ba0c6d5a28626963
ambrose <kolus7381@gmail.com>
2023-04-06 18:45:18 +0100

    gitty

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,10 +9,16 @@ int main(int argc, char __attribute__((unused)) *argv[])
 {
 	int a;
 	int res = 1;
-	
-	for (a = 1; a < argc; a++)
+	if (agrc >= 2)
 	{
-		res *= atoi(argv[a]);
+		for (a = 1; a < argc; a++)
+		{
+			res *= atoi(argv[a]);
+		}
+	}
+	else
+	{
+		return (-1);
 	}
 	printf("%d\n", res);
 	return (0);
