b03c38251ca7c53632799fb5b9c2b88773e68b05
ambrose <kolus7381@gmail.com>
2023-04-06 16:58:22 +0100

    new commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * main - a program that prints its name, followed by a new line
+  * @argc - argument count
+  * @argv - argument vector of 2d arrays
+  * Return - success 0;
+  */
+int main(int argc, char *argv[])
+{
+	_putchar(*argv[0]);
+	return (0);
+}
