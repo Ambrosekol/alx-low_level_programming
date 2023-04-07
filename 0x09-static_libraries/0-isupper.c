125ddf4855cdc7aa0b78d11d0f23279f130caf88
Ambrose <kolus7381@gmail.com>
2023-04-07 11:24:45 +0100

    new commit

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,6 @@
+#include "main.h"
+int _isupper(int __attribute__((unused)) c)
+{
+	return 0;
+}
+
---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,6 @@
+#include "main.h"
+char *_memset(char __attribute__((unused)) *s, char __attribute__((unused)) b, unsigned int __attribute__((unused)) n)
+{
+
+	return s;
+}
