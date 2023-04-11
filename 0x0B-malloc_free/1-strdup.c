565ff7242e4298fe9e980549d2bb282630c20219
ambrose <kolus7381@gmail.com>
2023-04-11 19:47:16 +0100

    new commit

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * _strdup - function that returns a pointer to a newly allocated space in memory,
+  * _strdup - function that returns a pointer to a array
   * @str: string
   * Return: char
   */
