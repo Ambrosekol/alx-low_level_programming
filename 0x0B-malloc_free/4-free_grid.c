43b6567cfbd5d965f2fe504cda0b8a2d3003da6e
ambrose <kolus7381@gmail.com>
2023-04-11 23:22:13 +0100

    new commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * free_grid - a function that frees a 2 dimensional grid 
+  * @grid: Grid to be freed
+  * @height: height of grid
+  * Return: Void
+  */
+void free_grid(int **grid, int height)
+{
+	int k;
+
+	if (grid == NULL || height <= 0)
+		return;
+
+	for (k = 0; k < height; k++)
+	{
+		free(grid[k]);
+	}
+	free(grid);
+}
