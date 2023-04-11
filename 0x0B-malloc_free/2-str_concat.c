d8f66b2f419e0b86cc8fbd97c0b67941dd627287
ambrose <kolus7381@gmail.com>
2023-04-11 21:42:44 +0100

    new commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+  * str_concat -  function that concatenates two strings.
+  * @s1: string 1
+  * @s2: string 2
+  * Return: char
+  */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, temp1, temp2, varhold;
+	char *k, *newptr1, *newptr2;
+
+	newptr1 = s1;
+	newptr2 = s2;
+	if (s1 == NULL)
+		newptr1 = "";
+	
+	if (s2 == NULL)
+		newptr2 = "";
+
+	temp1 = strlen(newptr1);
+	temp2 = strlen(newptr2);
+	varhold = temp1 + temp2;
+	k = malloc((sizeof(char) * temp1) + (sizeof(char) * temp2) + 1);
+	if (k == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < temp1; i++)
+	{
+		k[i] = newptr1[i];
+		varhold--;
+	}
+	for (j = varhold; j > 0; j--)
+	{
+		k[j] = newptr2[varhold - j];
+	}
+	return (k);
+}
+
