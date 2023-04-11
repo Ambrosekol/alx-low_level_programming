d0876bd6bfc76d7b2e8f7456018058692e4ca9a4
ambrose <kolus7381@gmail.com>
2023-04-11 23:14:21 +0100

    new commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,30 +7,28 @@
   */
 int **alloc_grid(int width, int height) /* 6 and 4 */
 {
-    int a, b;
-    int **value;
+	int a, b;
+	int **value;
 
-    if (width <= 0 || height <= 0)
-	    return (NULL);
-
-    value = malloc(sizeof(int *) * height);
-    if (value == NULL)
-	    return (NULL);
-
-    for (a = 0; a < height; a++)
-    {
-	    value[a] = malloc(sizeof(int) * width);
-	    if (value[a] == NULL)
-	    {
-		    for (b = 0; b < a; b++)
-			    free(value[b]);
-		    free(value);
-		    return (NULL);
-	    }
-	    for (b = 0; b < width; b++)
-	    {
-		    value[a][b] = 0;
-	    }
-    }
-    return (value);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	value = malloc(sizeof(int *) * height);
+	if (value == NULL)
+		return (NULL);
+	for (a = 0; a < height; a++)
+	{
+		value[a] = malloc(sizeof(int) * width);
+		if (value[a] == NULL)
+		{
+			for (b = 0; b < a; b++)
+				free(value[b]);
+			free(value);
+			return (NULL);
+		}
+		for (b = 0; b < width; b++)
+		{
+			value[a][b] = 0;
+		}
+	}
+	return (value);
 }
