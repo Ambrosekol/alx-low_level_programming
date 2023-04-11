da08e8c628f38dcab58a4e4cd4090a45085a2ce1
ambrose <kolus7381@gmail.com>
2023-04-11 17:54:34 +0100

    new commit

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,14 +13,14 @@ int main(int argc, char *argv[])
 	int rem = 0;
 	int realv;
 
-	realv = atoi(argv[1]);
-	if (argc != 2)
+	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
+		realv = atoi(argv[1]);
 		if (realv <= 0)
 		{
 			printf("%d\n", total);
