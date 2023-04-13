94ee05d8274866fb2698d10e0bd9619e5c9115a8
ambrose <kolus7381@gmail.com>
2023-04-13 22:15:10 +0100

    new commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,38 +3,29 @@
  * string_nconcat - function that concatenates two strings
  * @s1: input one
  * @s2: input two
- * @n: numbers
+ * @n: number of characters to concatenate from s2
  * Return: char
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int temp, i;
-	char *p1;
-	char *p2;
+	int s1_len, s2_len;
+	char *result;
 
-	p1 = s1;
-	p2 = s2;
-	temp = strlen(s1) - 1;
-	if (s2 == NULL)
-		p1 = "";
 	if (s1 == NULL)
-		p2 = "";
-	p1 = realloc(p1, sizeof(char) * (n + 1));
-	if (p1 != NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+	if ((int) n >= s2_len)
+		n = s2_len;
+	result = malloc(sizeof(char) * (s1_len + n + 1));
+	if (result == NULL)
 	{
-		exit (20);
+		return (NULL);
 	}
-	for (i = 0; i <= (int) n; i++)
-	{
-		if (i == (int) n)
-		{
-			p1[temp + 1] = '\0';
-		}
-		else
-		{
-			p1[temp + 1] = p2[i];
-		}
-		temp++;
-	}
-	return (p1);
+	strcpy(result, s1);
+	strncat(result, s2, n);
+	result[s1_len + n] = '\0';
+	return (result);
 }
