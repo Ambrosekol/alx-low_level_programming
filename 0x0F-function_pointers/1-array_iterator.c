bb1ffba3b71603c9f9f27904109a720a5917eff6
Ambrose <kolus7381@gmail.com>
2023-04-19 16:09:01 -0700

    new commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - iterate through an array
+ * @array: array to ilterate
+ * @size: size of array
+ * @action: void action
+ * Return: Void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
