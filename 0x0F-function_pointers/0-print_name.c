905cc69fe2342c9322e0ab0dad4449be9a3a6057
Ambrose <kolus7381@gmail.com>
2023-04-19 21:19:28 +0100

    new commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints name
+ * @name: pointer to name
+ * @(*f)(char *): pointer to function
+ * Return: viid
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
