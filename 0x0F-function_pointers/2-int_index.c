f1810acfb6ebb56de515acf134feded1c3f1a311
Ambrose <kolus7381@gmail.com>
2023-04-19 23:04:14 +0100

    new commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,12 @@
+/**
+ * int_index - index of a numner in array
+ * @array: array to use
+ * @size: size if array
+ * @(cmp)(int): pointer of function
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+		return (-1);
+
+}
