b0dee6a0adf02ad050953c2a49b55b166ebf0221
Ambrose <kolus7381@gmail.com>
2023-04-19 23:28:13 +0100

    new commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -8,5 +8,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	if (size <= 0)
 		return (-1);
-
+	for (i = 0, i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
 }
