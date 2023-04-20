3c43e04457fc2fd077b088a3facf3408442afde4
Ambrose <kolus7381@gmail.com>
2023-04-19 17:32:16 -0700

    new commit

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,5 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
+#endif
 /**
  * main -  program that prints the opcodes of its own main function.
  * @argc: number of arguments passed to the program
