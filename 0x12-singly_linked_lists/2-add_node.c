9332d79b8093a1afdf2b72584455c6bcaf73873e
Ambrose <kolus7381@gmail.com>
2023-04-27 23:50:08 +0100

    added code

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * add_node - adds a node
+ * @head: head of node
+ * @str: string to populate with
+ * Return: list_t
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	char *dupcate;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	dupcate = strdup(str);
+	if (dupcate == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->str = dupcate;
+	new_node->len = strlen(dupcate);
+	new_node->next = *head;
+	head = new_node;
+	return (new_node);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -18,4 +20,5 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
