b7c988944677061cf6ee2f07925dfe213b3a90ea
Ambrose <kolus7381@gmail.com>
2023-04-28 01:29:25 +0100

    new commit

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+    hello db 'Hello, Holberton', 0x0A, 0
+
+section .text
+    global main
+    extern printf
+
+main:
+    push rbp
+    mov rbp, rsp
+
+    lea rdi, [hello]
+    xor eax, eax
+    call printf
+
+    mov rsp, rbp
+    pop rbp
+    xor eax, eax
+    ret
