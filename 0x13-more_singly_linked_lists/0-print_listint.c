e8ec2f0672900cab0f65d2ea73a0a5195e2b94ca
ambrose <kolus7381@gmail.com>
2023-04-29 20:34:43 +0100

    nnew commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,10 +6,11 @@
   */
 size_t print_listint(const listint_t *h)
 {
-	size_t len = 0;
+	size_t len;
 
+	len = 0;
 	if (h == NULL)
-		return (NULL);
+		return (len);
 	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
