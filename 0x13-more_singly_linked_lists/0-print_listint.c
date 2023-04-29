6ba8960d90512fdb609dd5425932500760859988
ambrose <kolus7381@gmail.com>
2023-04-29 20:17:39 +0100

    new commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+  * print_listint - prints the contents of a linked list
+  * @h: pointer h to list
+  * Return: returns size of lists in type size_t
+  */
+size_t print_listint(const listint_t *h)
+{
+	size_t len = 0;
+
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
+		len++;
+		h = h->next;
+	}
+	if (h->next == NULL)
+	{
+		 printf("%d\n", h->n);
+		 len += 1;
+	}
+	return (len);
+}
