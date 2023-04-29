da44be0e61431302de72e2ed0a7aa6b5f6a0f577
ambrose <kolus7381@gmail.com>
2023-04-29 21:22:42 +0100

    new new commit

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdio.h>
 #include <stdlib>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -15,5 +17,6 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 #endif
