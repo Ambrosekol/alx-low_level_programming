17af86cf9bf2b7dc1e7411c96b4e19a23c58288f
Ambrose <kolus7381@gmail.com>
2023-05-01 07:20:56 -0700

    new commit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,11 +6,14 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
-
+	
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		tmp = *head;
 		*head = (*head)->next;
 		free(tmp);
 	}
+	*head = NULL;
 }
