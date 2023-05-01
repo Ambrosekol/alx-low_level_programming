e0896b9528aba30c4555411a354b582e6e57a5bb
Ambrose <kolus7381@gmail.com>
2023-05-01 07:52:54 -0700

    new commit

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - gets node index
+ * @head: head node
+ * @index: node to get
+ * Return: struct listint_t
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int chksum;
+	listint_t *temp;
+
+	chksum = 0;
+	temp = head;
+	while (chksum != index)
+	{
+		chksum++;
+		temp = head->next;
+	}
+	return (temp);
+}
