77c088c0afb79ec8e7db27e2e491541bfe728f48
Ambrose <kolus7381@gmail.com>
2023-05-01 14:19:59 +0100

    new commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - frees a linked list
+ * @head: head of list
+ * Returns: Void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free (tmp);
+	}
+}
