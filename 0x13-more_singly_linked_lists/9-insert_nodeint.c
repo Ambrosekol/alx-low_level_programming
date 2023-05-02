cb487a86e982901e9507b8ae8a855d5e30a09128
ambrose <kolus7381@gmail.com>
2023-05-02 12:20:11 +0100

    new commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	current = *head;
 	for (cnt = 0; cnt < (idx - 1); cnt++)
 	{
-		if (*current == NULL)
+		if (current == NULL)
 		{
 			free(node);
 			return (NULL);
