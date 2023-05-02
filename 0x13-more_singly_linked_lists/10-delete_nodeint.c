e3b9acddc966c71d7479a0a7e3634fbdbe5a8571
ambrose <kolus7381@gmail.com>
2023-05-02 13:07:57 +0100

    new commit

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,6 +10,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *holder, *temp, *mem;
 	unsigned int cnt;
 
+	mem = NULL;
 	if (head == NULL || *head == NULL)
 		return (-1);
 	cnt = 0;
