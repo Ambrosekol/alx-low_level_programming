147b0156ad2e33edb28acd3a274679307719e7b8
ambrose <kolus7381@gmail.com>
2023-05-02 12:27:24 +0100

    new commi

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (node == NULL)
 		return (NULL);
 	node->n = n;
+	if (*head == NULL && idx != 0)
+	{
+		free(node);
+		return (NULL);
+	}
 	if (idx == 0)
 	{
 		node->next = *head;
