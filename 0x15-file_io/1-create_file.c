8450c8260b1f4f7f0b66ecece0a0479447cb2c43
ambrose <kolus7381@gmail.com>
2023-05-08 19:08:20 +0100

    new commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * create_file - Creates a file
+  * @filename: name of the file
+  * @text_content: contents of the text
+  * Return: returns 1 on success or -1 if otherwise
+  */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd;
+	size_t file_len;
+
+	file_len = 0;
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O-TRUNC, S_IRUSR | S_IWUSR);
+	if (fd < 0)
+		return (-1);
+	if (text_content != NULL)
+		file_len = strlen(text_content);
+	status = write(fd, text_content, file_len);
+	if (status < 0)
+		return (-1);
+	close(fd);
+	return (status);
+}
