6a221ac9c8fa4d5024fb903d683382e9f03bae34
ambrose <kolus7381@gmail.com>
2023-05-08 18:38:08 +0100

    nnew commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 /**
   * read_textfile - Read a text file and prints to stdout
   * @filename: pointer to name of file
-  * @letters amount of letters to print out
+  * @letters: amount of letters to print out
   * Return: return amount of letters printed in ssize_t
   */
 ssize_t read_textfile(const char *filename, size_t letters)
