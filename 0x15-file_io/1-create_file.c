3ac07d9574bdc6721be51ae300c8ee0a3f020c1e
ambrose <kolus7381@gmail.com>
2023-05-08 19:09:42 +0100

    new commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,8 +13,7 @@ int create_file(const char *filename, char *text_content)
 	file_len = 0;
 	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_WRONLY | O_CREAT | O-TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd < 0)
 		return (-1);
 	if (text_content != NULL)
