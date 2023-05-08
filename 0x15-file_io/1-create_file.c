c6f4db4d584b9bda47899c1f256b9a0884a9ee12
ambrose <kolus7381@gmail.com>
2023-05-08 19:16:57 +0100

    new commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
   */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t fd;
+	ssize_t fd, status;
 	size_t file_len;
 
 	file_len = 0;
@@ -22,5 +22,5 @@ int create_file(const char *filename, char *text_content)
 	if (status < 0)
 		return (-1);
 	close(fd);
-	return (status);
+	return ((int) status);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 #include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
