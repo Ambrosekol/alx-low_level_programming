2bea2d694457c3169364f6525977e3ab442127d6
ambrose <kolus7381@gmail.com>
2023-07-06 21:06:23 +0100

    new commit

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,7 @@
 #ifndef HASH_H
 #define HASH_H
 #include <stdlib.h>
+
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -11,9 +12,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -26,8 +27,9 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
+
 hash_table_t *hash_table_create(unsigned long int size);
 #endif
