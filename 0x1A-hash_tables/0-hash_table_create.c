9d345fb35fe00a6dca58951f10e71ce4d41bc096
ambrose <kolus7381@gmail.com>
2023-07-06 20:09:46 +0100

    removed a code

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	hash_node_t *buckets;
 
-	if (size == 0)
-		return (NULL);
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
