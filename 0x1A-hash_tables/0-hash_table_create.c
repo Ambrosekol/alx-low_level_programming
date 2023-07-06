7e740388b240dd01f6c1d52628263740adeafd69
ambrose <kolus7381@gmail.com>
2023-07-06 19:28:00 +0100

    initiated new directory. Hash Tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,11 @@
+#include "hash_tables.h"
+/**
+  * hash_table_create - This function creates an Hash table
+  * @size: This is the size of the Hash Table to be created
+  * Return: A new Hash table
+  */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+}
