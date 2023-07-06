4598164fda149ae0bff7c6054b74117b4679b5ee
ambrose <kolus7381@gmail.com>
2023-07-06 20:29:42 +0100

    new commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,20 +8,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	hash_node_t *buckets;
 	unsigned long int i;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
+
 	new_table->size = size;
-	buckets = malloc(sizeof(hash_node_t) * size);
-	if (buckets == NULL)
+	new_table->array = malloc(sizeof(hash_node_t) * size);
+	if (new_table->array == NULL)
 	{
-		free(new_table);
 		return (NULL);
 	}
-	new_table->array = &buckets;
 	for (i = 0; i < size; i++)
 	{
 		new_table->array[i] = NULL;
