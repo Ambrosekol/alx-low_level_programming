4cc06aae078bdb19271e88728cbc71789ec227e9
ambrose <kolus7381@gmail.com>
2023-07-07 19:37:34 +0100

    new code changes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,34 +9,43 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *node, *cursor;
-	char buffer1[1024];
+	hash_node_t *new;
+	char *value_cpy;
+	unsigned long int index, i;
 
-	if (key == NULL || ht == NULL || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
+	value_cpy = strdup(value);
+	if (value_cpy == NULL)
 		return (0);
-
-	node->key = (char *)key;
-	strcpy(buffer1, value);
-	node->value = buffer1;
-	node->next = NULL;
 
 	index = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
+	for (i = index; ht->array[i]; i++)
 	{
-		ht->array[index] = node;
-		return (1);
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_cpy;
+			return (1);
+		}
 	}
 
-	cursor = ht->array[index];
-	while (cursor->next != NULL)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
 	{
-		cursor = cursor->next;
+		free(value_cpy);
+		return (0);
 	}
-	cursor->next = node;
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_cpy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
 	return (1);
 }
