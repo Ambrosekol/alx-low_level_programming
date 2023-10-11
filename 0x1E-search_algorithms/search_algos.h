929964408ec3c92a6bc74fdd60a46c2d8281cb5c
ambrose <kolus7381@gmail.com>
2023-10-12 00:32:10 +0100

    new

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,5 +4,5 @@
 #include <stdio.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int _binary_search(int *array, size_t left, size_t right, int value);
 #endif
